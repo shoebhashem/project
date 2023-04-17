450d8ad9265013f72b300f69015becb04f2b6642
shashem1 <shoeb.hashem@volvocars.com>
2023-04-17 11:16:10 +0200

    jenkinsfiles and mvn-setting file added

--- src/main/java/Agile/Software/Testing/LeanTesting/Divide.java
+++ src/main/java/Agile/Software/Testing/LeanTesting/Divide.java
@@ -1,5 +1,10 @@
 package Agile.Software.Testing.LeanTesting;
 
 public class Divide {
+	
+	 public static double divide(int num_1, int num_2)
+	    {
+	        return ((double) num_1 / num_2);
+	    }
 
 }
